22aaf0e76b0791818647d21bf8d2c4fdfcf64da5
roorco <roorco@gmail.com>
2021-01-16 20:06:46 +0100

    Remove newly ignored directories

--- cache/compiled/files/1819093404e159b2e02e30d5beca1f80.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/srv/http/S3L/user/plugins/auto-date/auto-date.yaml',
-    'modified' => 1610747576,
-    'data' => [
-        'enabled' => true
-    ]
-];
--- cache/compiled/files/cd5492611a2250eb248b455564da52a9.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/srv/http/S3L/user/config/security.yaml',
-    'modified' => 1610743997,
-    'data' => [
-        'salt' => 'obR2sHqDKltSW3'
-    ]
-];
